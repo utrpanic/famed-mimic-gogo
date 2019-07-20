7be4558931dfe5463b83129ea7dc6716e97c4675
box.jeon <utrpanic@gmail.com>
2019-07-20 11:53:51 +0900

    ShazamBoomBoom initial commit.

---
+++ ShazamBoomBoom/ShazamBoomBoom/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ShazamBoomBoom
+//
+//  Created by gurren-l-macbook-pro on 20/07/2019.
+//  Copyright © 2019 utrpanic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
