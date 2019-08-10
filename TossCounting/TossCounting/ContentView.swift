3fa629001ef81bd72c77a2f3679b2b4878a70c53
BoxJeon <utrpanic@gmail.com>
2019-08-10 23:28:43 +0900

    Testing Import Swift Package Manager .

---
+++ TossCounting/TossCounting/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TossCounting
+//
+//  Created by BoxJeon on 2019/08/10.
+//  Copyright © 2019 utrpanic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
